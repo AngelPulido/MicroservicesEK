85200a93493f2af59accebd27e42eaf7e4e7195b
AngelPulido <100270026+AngelPulido@users.noreply.github.com>
2025-09-01 11:49:43 -0600

    Delete Microservices.Ek.Query,Application directory

--- Microservices.Ek.Query,Application/Models/KafkaSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microservices.Ek.Query_Application.Models
-{
-    public class KafkaSettings
-    {
-        public string GroupId { get; set; } = string.Empty;
-        public string Hostname {  get; set; } = string.Empty;
-        public string Port {  get; set; } = string.Empty;
-    }
-}
